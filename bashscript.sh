f57802861c3f9650b393e770cdd50264b84fe208
Meghna1207 <106789481+Meghna1207@users.noreply.github.com>
2023-04-10 14:21:19 +1000

    Update bashscript.sh

--- bashscript.sh
+++ bashscript.sh
@@ -3,8 +3,8 @@
 # Get disk usage percentage
 disk_usage=$(df -h / | awk 'NR==2{print $5}' | cut -d'%' -f1)
 
-# Check if disk usage is greater than 30%
-if [ "$disk_usage" -gt 30 ]; then
+# Check if disk usage is greater than 80%
+if [ "$disk_usage" -gt 80 ]; then
                   # Log current time to diskusage.log file
                   echo $(date) $disk_usage >> /home/meghashah/diskusage.log
 fi
